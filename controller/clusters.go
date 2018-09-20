2bec1e3c40f6f9992a0a1e5fb55e372bf005b0b0
Alexey Kazakov <alkazako@redhat.com>
2018-09-19 17:21:50 -0700

    Allow Auth SA to access full cluster configuration

--- controller/clusters.go
+++ controller/clusters.go
@@ -6,6 +6,7 @@ import (
 	"github.com/fabric8-services/fabric8-cluster/jsonapi"
 	"github.com/fabric8-services/fabric8-cluster/rest"
 	"github.com/fabric8-services/fabric8-cluster/token"
+
 	"github.com/fabric8-services/fabric8-common/errors"
 	"github.com/fabric8-services/fabric8-common/log"
 
@@ -30,9 +31,9 @@ func NewClustersController(service *goa.Service, config clusterConfiguration) *C
 	}
 }
 
-// Show runs the list of available OSO clusters.
+// Show returns the list of available OSO clusters.
 func (c *ClustersController) Show(ctx *app.ShowClustersContext) error {
-	if !token.IsSpecificServiceAccount(ctx, token.OsoProxy, token.Tenant, token.JenkinsIdler, token.JenkinsProxy) {
+	if !token.IsSpecificServiceAccount(ctx, token.OsoProxy, token.Tenant, token.JenkinsIdler, token.JenkinsProxy, token.Auth) {
 		log.Error(ctx, nil, "unauthorized access to cluster info")
 		return jsonapi.JSONErrorResponse(ctx, errors.NewUnauthorizedError("unauthorized access to cluster info"))
 	}
@@ -50,6 +51,39 @@ func (c *ClustersController) Show(ctx *app.ShowClustersContext) error {
 		data = append(data, cluster)
 	}
 	clusters := app.ClusterList{
+		Data: data,
+	}
+	return ctx.OK(&clusters)
+}
+
+// ShowAuthClient returns the list of available OSO clusters with full configuration including Auth client data.
+// To be used by Auth service only
+func (c *ClustersController) ShowAuthClient(ctx *app.ShowAuthClientClustersContext) error {
+	if !token.IsSpecificServiceAccount(ctx, token.Auth) {
+		log.Error(ctx, nil, "unauthorized access to cluster info")
+		return jsonapi.JSONErrorResponse(ctx, errors.NewUnauthorizedError("unauthorized access to cluster info"))
+	}
+	var data []*app.FullClusterData
+	for _, clusterConfig := range c.config.GetOSOClusters() {
+		cluster := &app.FullClusterData{
+			Name:              clusterConfig.Name,
+			APIURL:            rest.AddTrailingSlashToURL(clusterConfig.APIURL),
+			ConsoleURL:        rest.AddTrailingSlashToURL(clusterConfig.ConsoleURL),
+			MetricsURL:        rest.AddTrailingSlashToURL(clusterConfig.MetricsURL),
+			LoggingURL:        rest.AddTrailingSlashToURL(clusterConfig.LoggingURL),
+			AppDNS:            clusterConfig.AppDNS,
+			CapacityExhausted: clusterConfig.CapacityExhausted,
+
+			AuthClientDefaultScope: clusterConfig.AuthClientDefaultScope,
+			AuthClientID:           clusterConfig.AuthClientID,
+			AuthClientSecret:       clusterConfig.AuthClientSecret,
+			ServiceAccountToken:    clusterConfig.ServiceAccountToken,
+			ServiceAccountUsername: clusterConfig.ServiceAccountUsername,
+			TokenProviderID:        clusterConfig.TokenProviderID,
+		}
+		data = append(data, cluster)
+	}
+	clusters := app.FullClusterList{
 		Data: data,
 	}
 	return ctx.OK(&clusters)
--- controller/clusters_blackbox_test.go
+++ controller/clusters_blackbox_test.go
@@ -34,6 +34,7 @@ func (s *ClustersTestSuite) TestShowForServiceAccountsOK() {
 	s.checkShowForServiceAccount("fabric8-tenant")
 	s.checkShowForServiceAccount("fabric8-jenkins-idler")
 	s.checkShowForServiceAccount("fabric8-jenkins-proxy")
+	s.checkShowForServiceAccount("fabric8-auth")
 }
 
 func (s *ClustersTestSuite) checkShowForServiceAccount(saName string) {
@@ -66,4 +67,48 @@ func (s *ClustersTestSuite) TestShowForUnknownSAFails() {
 	}
 	service, controller := s.SecuredControllerWithServiceAccount(sa)
 	test.ShowClustersUnauthorized(s.T(), service.Context, service, controller)
+}
+
+func (s *ClustersTestSuite) TestShowForAuthServiceAccountsOK() {
+	require.True(s.T(), len(s.Config.GetOSOClusters()) > 0)
+	s.checkShowAuthForServiceAccount("fabric8-auth")
+}
+
+func (s *ClustersTestSuite) TestShowAuthForUnknownSAFails() {
+	sa := &testsupport.Identity{
+		Username: "fabric8-tenant",
+		ID:       uuid.NewV4(),
+	}
+	service, controller := s.SecuredControllerWithServiceAccount(sa)
+	test.ShowAuthClientClustersUnauthorized(s.T(), service.Context, service, controller)
+}
+
+func (s *ClustersTestSuite) checkShowAuthForServiceAccount(saName string) {
+	sa := &testsupport.Identity{
+		Username: saName,
+		ID:       uuid.NewV4(),
+	}
+	service, controller := s.SecuredControllerWithServiceAccount(sa)
+	_, clusters := test.ShowAuthClientClustersOK(s.T(), service.Context, service, controller)
+	require.NotNil(s.T(), clusters)
+	require.NotNil(s.T(), clusters.Data)
+	require.Equal(s.T(), len(s.Config.GetOSOClusters()), len(clusters.Data))
+	for _, cluster := range clusters.Data {
+		configCluster := s.Config.GetOSOClusterByURL(cluster.APIURL)
+		require.NotNil(s.T(), configCluster)
+		require.Equal(s.T(), configCluster.Name, cluster.Name)
+		require.Equal(s.T(), rest.AddTrailingSlashToURL(configCluster.APIURL), cluster.APIURL)
+		require.Equal(s.T(), rest.AddTrailingSlashToURL(configCluster.ConsoleURL), cluster.ConsoleURL)
+		require.Equal(s.T(), rest.AddTrailingSlashToURL(configCluster.MetricsURL), cluster.MetricsURL)
+		require.Equal(s.T(), rest.AddTrailingSlashToURL(configCluster.LoggingURL), cluster.LoggingURL)
+		require.Equal(s.T(), configCluster.AppDNS, cluster.AppDNS)
+		require.Equal(s.T(), configCluster.CapacityExhausted, cluster.CapacityExhausted)
+
+		require.Equal(s.T(), configCluster.AuthClientDefaultScope, cluster.AuthClientDefaultScope)
+		require.Equal(s.T(), configCluster.AuthClientID, cluster.AuthClientID)
+		require.Equal(s.T(), configCluster.AuthClientSecret, cluster.AuthClientSecret)
+		require.Equal(s.T(), configCluster.ServiceAccountToken, cluster.ServiceAccountToken)
+		require.Equal(s.T(), configCluster.ServiceAccountUsername, cluster.ServiceAccountUsername)
+		require.Equal(s.T(), configCluster.TokenProviderID, cluster.TokenProviderID)
+	}
 }
